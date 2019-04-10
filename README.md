cb9880e049539a89839ae49fbe19a6ee3f395fd4
Liwei Huang <aa583045912@gmail.com>
2019-04-10 01:05:55 +0000

    more work

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # soundCloud-project
 
 # Purpose
-1. This project needs to play songs
-2. Search from soundcloud
-3. Make a playlist
+## key works
+1. Menu on the top row should has Recommand, Search, and Setting buttons
+2. Move the searchBar so it is inside the search part
+## extras
+1. This project needs to play songs(done)
+2. Search from soundcloud(done)
+3. Make a playlist(half way)
 4. Give it play/pause and volume buttons
 5. Make it look good
 
-Link: 
+Link: https://docs.google.com/document/d/1MHh5vNQipOH0QtGr8J-PhEu3LTi3eUcn5xjIEx2IYaI/edit
--- script.js
+++ script.js
@@ -1,18 +1,90 @@
-/* global $ */
+/* global $ SC */
 
-$('#search').click(function(evt){
-    let input = $('searchBar').val();
+SC.initialize({
+  client_id: '5aa8e389ba4e24b6106af5159ab3e344',
+  redirect_uri: 'http://google.com'
+});
+
+let songs = [];
+let playList = [];
+
+$('#searcher').click(function(evt){
+    let input = $('#searchBar').val();
+    songs = [];
+    console.log(input);
     $.ajax({
         url: `https://api.soundcloud.com/tracks?q=${input}&client_id=5aa8e389ba4e24b6106af5159ab3e344`,
         method: "GET",
         success: function(data){
             console.log(data);
+            if (data.length === 0){
+              return;
+            }
+            //stream all
+            let promises = [];
+            data.forEach(function(v, i){
+              SC.stream(`/tracks/${v.id}`).then(function(player){
+                console.log(player);
+                songs.push(player);
+                //try to play
+                promises.push(
+                  player.play().then(function(){
+                    console.log('Playback started!');
+                    player.pause();
+                  }).catch(function(e){
+                    console.error('Playback rejected.', e, i);
+                    songs.splice(i, 1);
+                    //also splice from data
+                    data.splice(i, 1);
+                  })
+                );
+              });
+            });
+            // console.log(promises);
+            // Promise.all(promises).then(function(values) {
+            //   //pushing data
+            //   console.log('splicing done');
+            //   console.log(data);
+            // });
+            
         }
     });
 });
 
-function play(url){
-    let audio = new Audio();
-    audio.url = url;
-    audio.play();
-}
+$(".topButtons").click(function(){
+  let id = $(this).attr('id');
+  $('.contents').hide();
+  if (id === 'searchButton'){
+    $('#searchContent').show();
+  }else if (id === 'recommandedButton'){
+    $('#recommendContent').show();
+  }else{
+    $('#settingContent').show();
+  }
+});
+//hide everything
+$('.contents').hide();
+
+
+// let cover = [];
+
+// $("#bottom").click(function(){
+//   $.ajax({
+//     ur:`https://api.soundcloud.com/tracks?&client_id=5aa8e389ba4e24b6106af5159ab3e344`,
+//     method:"GET",
+//     success: function(response){
+      
+        
+      
+      
+      
+//     }
+//   });
+  
+  
+  
+  
+  
+  
+  
+// });

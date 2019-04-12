a999887444ffb30cda5457b472909e668e357cc2
Liwei Huang <aa583045912@gmail.com>
2019-04-12 18:24:54 +0000

    added a pic, also improved css

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ SC.initialize({
 });
 
 let songs = [];
+let songData = [];
 let playList = [];
 
 $('#searcher').click(function(evt){
@@ -21,25 +22,67 @@ $('#searcher').click(function(evt){
               return;
             }
             //stream all
-            let promises = [];
+            let playerList = [];
+            let idList = [];
             data.forEach(function(v, i){
-              SC.stream(`/tracks/${v.id}`).then(function(player){
-                console.log(player);
-                songs.push(player);
-                //try to play
-                promises.push(
-                  player.play().then(function(){
-                    console.log('Playback started!');
-                    player.pause();
-                  }).catch(function(e){
-                    console.error('Playback rejected.', e, i);
-                    songs.splice(i, 1);
-                    //also splice from data
-                    data.splice(i, 1);
-                  })
-                );
-              });
+              idList.push(v.id);
+            //   promises.push(SC.stream(`/tracks/${v.id}`).then(function(player){
+            //     playerList.push(player);
+            //     console.log(player);
+            //     songs.push(player);
+            //     //try to play
+            //     player.play().then(function(){
+            //       //successful play
+            //       console.log('Playback started!');
+            //       player.pause();
+            //       //push to screen
+                  
+            //     }).catch(function(e){
+            //       console.error('Playback rejected.', e, i);
+            //       songs.splice(i, 1);
+            //     })
+            //   }));
+            // });
+            // Promise.all(promises).then(function(){
+            //   console.log(playerList);
+            //   songs = playerList;
+            //   flow(playerList);
             });
+            //grabs lets go
+            async function flow(ids){
+              //grabs all id of songs get and send request to get audio files
+              for (let i = 0; i < ids.length; i ++){
+                await SC.stream(`/tracks/${ids[i]}`).then(function(p){
+                  playerList.push(p);
+                });
+              }
+              let audios = playerList;
+              //play every audio to see if they dead or not
+              for(let i = 0; i < audios.length; i ++) {
+                let audio = audios[i];
+                await audio.play().then(function(){
+                  //success
+                  console.log('Playback started!');
+                  audio.pause();
+                  songs.push(audio);
+                }).catch(function(e){
+                  //fail and pop
+                  console.error('Playback rejected.', e, i);
+                  songs.splice(i, 1);
+                  data.splice(i, 1);
+                });
+              }
+              songData = data;
+              console.log(data);
+              console.log(songs);
+              //rendering
+              for (let i = 0; i < songData; i ++){
+                let data = songData[i];
+                
+              }
+            }
+            //run
+            flow(idList);
             // console.log(promises);
             // Promise.all(promises).then(function(values) {
             //   //pushing data

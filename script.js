2cddadebecbe31cc97e459ef1b70edb10218a59a
Liwei Huang <aa583045912@gmail.com>
2019-04-24 05:43:08 +0000

    finishing touch

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ SC.initialize({
 let songs = [];
 let songData = [];
 let playList = [];
+let currentSong;
+
+let $slider = $('.slider');
 
 $('#searcher').click(function(evt){
     let input = $('#searchBar').val();
     songs = [];
     console.log(input);
+    let $output = $('#searchOutPut');
+    $('#searchOutPut').empty();
+    $("#searchOutPut").css("background","url(imgs/loading.gif) no-repeat center");
     $.ajax({
         url: `https://api.soundcloud.com/tracks?q=${input}&client_id=5aa8e389ba4e24b6106af5159ab3e344`,
         method: "GET",
@@ -76,10 +82,53 @@ $('#searcher').click(function(evt){
               console.log(data);
               console.log(songs);
               //rendering
-              for (let i = 0; i < songData; i ++){
+              for (let i = 0; i < songData.length; i ++){
                 let data = songData[i];
-                
+                console.log(data);
+                let $song = $('<div>').addClass(i).addClass('songDiv');
+                $output.append($song);
+                //img
+                let $img = $('<img>');
+                if(data.artwork_url !== null){
+                  $img.attr('src', data.artwork_url).addClass("songImg");
+                }else{
+                  $img.attr('src', "imgs/noImg.png").addClass("songImg");
+                }
+                $song.append($img);
+                //title
+                let $name = $('<div>');
+                $name.text(data.title).addClass("songName");
+                $song.append($name);
+                //artist
+                let $artist = $("<div>");
+                $artist.text(data.user.username).addClass("songArtist");
+                $song.append($artist);
+                //genre
+                let $genre = $('<div>');
+                $genre.text(data.genre);
+                $song.append($artist);
+                //description
+                let $desc = $('<div>')
+                  $desc.addClass("desc");
+            
+                $desc.text(data.description).addClass("songDesc");
+                $song.append($desc);
+                //play button
+                let $button = $('<img>').attr('src', 'imgs/someSort_Logo.png').addClass('playButton');
+                let buttonContainer = $('<div>');
+                  //append pic into ^
+                    buttonContainer.append($button);
+                  //lol that's it why i comment here lol?
+                  
+                $button.attr('class', i);
+                $button.click(function(){
+                  playSong(parseInt(i));
+                  playList.push(parseInt(i));
+                })
+                $song.append(buttonContainer);
               }
+              //remove loading gif here
+                  $("#searchOutPut").css("background","");
             }
             //run
             flow(idList);
@@ -89,7 +138,6 @@ $('#searcher').click(function(evt){
             //   console.log('splicing done');
             //   console.log(data);
             // });
-            
         }
     });
 });
@@ -108,7 +156,136 @@ $(".topButtons").click(function(){
 //hide everything
 $('.contents').hide();
 
+let songTime = 0;
+let songLength = Infinity;
+let playing = false;
 
+//timer from stackoverflow
+var interval = 1000; // ms
+var expected = Date.now() + interval;
+setTimeout(step, interval);
+function step() {
+    var dt = Date.now() - expected; // the drift (positive for overshooting)
+    //do stuff if playing
+    if (playing){
+      //add time
+      songTime += interval;
+      //check if song end
+      if (songTime > songLength){
+        nextSong();
+      }
+      //render according to time
+      $slider.val(songTime / 1000);
+      //render text
+      let second = songTime / 1000;
+      let str = String(second % 60);
+      if (str.length === 1){
+        str = "0" + str;
+      }
+      $('#cTime').text(`${Math.floor(second / 60)}:${str}`);
+    }
+    expected += interval;
+    setTimeout(step, Math.max(0, interval - dt)); // take into account drift
+}
+
+function playSong(id){
+  $("#playPause").attr("src","imgs/pause.png");
+  let song = songs[id];
+  songLength = song.getDuration();
+  song.play();
+  playing = true;
+  currentSong = id;
+  songTime = 0;
+  //set scroll bar
+  let second = Math.ceil(song.getDuration() / 1000);
+  $slider.attr('min', '0');
+  $slider.attr('max', second);
+  $slider.val(0);
+  let str = String(second % 60);
+  if (str.length === 1){
+    str = "0" + str;
+  }
+  $('#sTime').text(`${Math.floor(second / 60)}:${str}`);
+}
+
+function moveSong(){
+  let value = $slider.val();
+  songTime = value * 1000;
+  songs[currentSong].seek(songTime);
+}
+
+function nextSong(){
+  //if next song exist
+  if (playList[currentSong + 1]){
+    currentSong += 1;
+  }else{
+    currentSong = 0;
+  }
+  playSong(currentSong);
+}
+
+//songList
+let playListShow = false;
+let $list = $('#playList');
+$list.hide();
+$('#songList').click(function(evt){
+  if (playListShow){
+    $list.hide();
+    playListShow = false;
+  }else{
+    $list.show();
+    playListShow = true;
+  }
+  $("#playListSongs").empty();
+  //append songs
+  let f = function(v, i) {
+    let $song = $('<div>').addClass('playListSong');
+    $song.attr('index', i);
+    if (i === currentSong){
+      $song.addClass('currentSong');
+    }
+    $("#playListSongs").append($song);
+    let data = songData[v];
+    let song = songs[v];
+    let $name = $("<div>").text(data.title);
+    $song.append($name);
+    //handler
+    $song.click(function(){
+      if (i !== currentSong){
+        playSong(i);
+        $("#playListSongs").empty();
+        playList.forEach(f);
+      }
+    });
+  }
+  playList.forEach(f);
+  //position it
+  $list.offset({left: evt.pageX - $list.width() - 10, top: evt.pageY - 10 - $list.height()});
+});
+$('.playListSong').on('contextmenu', function(e){
+  //prevent context menu
+  e.preventDefault();
+  let i = $(this).attr('index');
+  //if last song on list
+  if (playList.length === 1){
+    songs[currentSong].pause();
+    playing = false;
+    playList = [];
+    currentSong = undefined;
+    return;
+  }
+  //if the end of the list
+  if (i === playList.length - 1){
+    //play previous song
+    playSong(i - 1);
+    //remove from list
+    playList.pop();
+    return;
+  }
+  //other wise
+  playList.splice(i, 1);
+  playSong(i);
+});
 // let cover = [];
 
 // $("#bottom").click(function(){
@@ -123,11 +300,20 @@ $('.contents').hide();
       
 //     }
 //   });
-  
-  
-  
-  
-  
-  
-  
 // });
+
+//pause and play and what not---------------------------------------------------
+$("#playPause").click(function() {
+  if(playing === true){
+    songs[currentSong].pause();
+    playing = false;
+    $("#playPause").attr("src","imgs/play.png");
+  }else if(playing === false && currentSong){
+    songs[currentSong].play();
+    playing = true;
+    $("#playPause").attr("src","imgs/pause.png");
+  }else{//else may be useless here, dont know, didn't try
+    $("#playPause").attr("src","imgs/loading.gif");
+  }
+  
+});
